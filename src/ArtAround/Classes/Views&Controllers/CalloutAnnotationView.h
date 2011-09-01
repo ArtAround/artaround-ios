8388b1f3994376f2bfeb06f1c456b071d6b8a6ea
Brandon Jones <btjones@gmail.com>
2011-08-31 21:42:59 -0400

    show art details in the pin callout (photo not done yet)

--- src/ArtAround/Classes/Views&Controllers/CalloutAnnotationView.h
+++ src/ArtAround/Classes/Views&Controllers/CalloutAnnotationView.h
@@ -10,14 +10,20 @@
 #import <MapKit/MapKit.h>
 @class Art;
 @class ArtAnnotationView;
+@class EGOImageView;
 
 @interface CalloutAnnotationView : MKAnnotationView <MKAnnotation> 
 
-@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
 @property (nonatomic, assign) Art *art;
-@property (nonatomic, retain) UIButton *button;
 @property (nonatomic, assign) ArtAnnotationView *parentAnnotationView;
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
 @property (nonatomic, retain) MKMapView *mapView;
+@property (nonatomic, retain) UIButton *button;
+@property (nonatomic, retain) EGOImageView *imageView;
+@property (nonatomic, retain) UILabel *titleLabel;
+@property (nonatomic, retain) UILabel *artistLabel;
+@property (nonatomic, retain) UILabel *categoryLabel;
+@property (nonatomic, retain) UILabel *summaryLabel;
 
 - (id)initWithCoordinate:(CLLocationCoordinate2D)theCoordinate frame:(CGRect)frame;
 
--- src/ArtAround/Classes/Views&Controllers/CalloutAnnotationView.m
+++ src/ArtAround/Classes/Views&Controllers/CalloutAnnotationView.m
@@ -10,13 +10,19 @@
 #import "Art.h"
 #import "ArtAnnotation.h"
 #import "ArtAnnotationView.h"
+#import "Category.h"
+#import "Photo.h"
+#import "EGOImageView.h"
+#import "FlickrAPIManager.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface CalloutAnnotationView (private)
 - (void)preventParentSelectionChange;
 @end
 
 @implementation CalloutAnnotationView
-@synthesize coordinate = _coordinate, art = _art, button = _button, parentAnnotationView = _parentAnnotationView, mapView = _mapView;
+@synthesize coordinate = _coordinate, art = _art, parentAnnotationView = _parentAnnotationView, mapView = _mapView;
+@synthesize button = _button, imageView = _imageView, titleLabel = _titleLabel, artistLabel = _artistLabel, categoryLabel = _categoryLabel, summaryLabel = _summaryLabel;
 
 - (id)initWithCoordinate:(CLLocationCoordinate2D)theCoordinate frame:(CGRect)frame
 {
@@ -34,6 +40,49 @@
 		[self setButton:aButton];
 		[self addSubview:self.button];
 		
+		//image
+		EGOImageView *anImageView = [[EGOImageView alloc] initWithPlaceholderImage:nil];
+		[anImageView setBackgroundColor:[UIColor lightGrayColor]];
+		[anImageView setFrame:CGRectMake(13.0f, 13.0f, 100.0f, 100.0f)];
+		[anImageView.layer setBorderWidth:2.0f];
+		[anImageView.layer setBorderColor:[UIColor whiteColor].CGColor];
+		[self setImageView:anImageView];
+		[self addSubview:self.imageView];
+		[anImageView release];
+		
+		//title
+		UILabel *aTitleLabel = [[UILabel alloc] init];
+		[aTitleLabel setFont:[UIFont fontWithName:@"Georgia-Bold" size:13.0f]];
+		[aTitleLabel setBackgroundColor:[UIColor clearColor]];
+		[self setTitleLabel:aTitleLabel];
+		[self addSubview:self.titleLabel];
+		[aTitleLabel release];
+	
+		//artist
+		UILabel *anArtistLabel = [[UILabel alloc] init];
+		[anArtistLabel setFont:[UIFont fontWithName:@"Helvetica" size:9.25f]];
+		[anArtistLabel setBackgroundColor:[UIColor clearColor]];
+		[self setArtistLabel:anArtistLabel];
+		[self addSubview:self.artistLabel];
+		[anArtistLabel release];
+		
+		//category
+		UILabel *aCategoryLabel = [[UILabel alloc] init];
+		[aCategoryLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:9.25f]];
+		[aCategoryLabel setBackgroundColor:[UIColor clearColor]];
+		[self setCategoryLabel:aCategoryLabel];
+		[self addSubview:self.categoryLabel];
+		[aCategoryLabel release];
+		
+		//summary
+		UILabel *aSummaryLabel = [[UILabel alloc] init];
+		[aSummaryLabel setFont:[UIFont fontWithName:@"Helvetica" size:9.25f]];
+		[aSummaryLabel setBackgroundColor:[UIColor clearColor]];
+		[aSummaryLabel setNumberOfLines:3];
+		[self setSummaryLabel:aSummaryLabel];
+		[self addSubview:self.summaryLabel];
+		[aSummaryLabel release];
+
     }
     
     return self;
@@ -42,11 +91,65 @@
 - (void)dealloc
 {
 	[self setButton:nil];
+	[self setImageView:nil];
+	[self setTitleLabel:nil];
+	[self setArtistLabel:nil];
+	[self setCategoryLabel:nil];
+	[self setSummaryLabel:nil];
 	[super dealloc];
+}
+
+- (void)prepareForReuse
+{
+	[self setArt:nil];
 }
 
 - (void)setArt:(Art *)art
 {
+	//assign the art
+	_art = art;
+
+	//if art is nil, reset everything
+	if (!_art) {
+		[self.imageView setImageURL:nil];
+		[self.titleLabel setText:@""];
+		[self.artistLabel setText:@""];
+		[self.categoryLabel setText:@""];
+		[self.summaryLabel setText:@""];
+		return;
+	}
+	
+	//are the fields empty?
+	BOOL showTitle = _art.title && ![_art.title isEqualToString:@""];
+	BOOL showArtist = _art.artist && ![_art.artist isEqualToString:@""];
+	BOOL showYear = _art.year && ![_art.year intValue] == 0;
+	
+	//artist label is a concatenation of artist - year
+	NSString *artist = @"";
+	if (showArtist && showYear) {
+		artist = [NSString stringWithFormat:@"%@ - %@", _art.artist, _art.year];
+	} else if (showArtist && !showYear) {
+		artist = _art.artist;
+	} else if (!showArtist && showYear) {
+		artist = [_art.year stringValue];
+	}
+	
+	//set label text
+	[self.titleLabel setText:_art.title];
+	[self.artistLabel setText:artist];
+	[self.categoryLabel setText:_art.category.title];
+	[self.summaryLabel setText:_art.locationDescription];
+	
+	//update frames
+	const float padding = 10.0f;
+	float yOffset = self.imageView.frame.origin.y;
+	[self.titleLabel setFrame:CGRectMake(self.imageView.frame.origin.x + self.imageView.frame.size.width + padding, yOffset, 185.0f, 20.0f)];
+	yOffset = (showTitle) ? self.titleLabel.frame.origin.y + self.titleLabel.frame.size.height : yOffset;
+	[self.artistLabel setFrame:CGRectMake(self.titleLabel.frame.origin.x, yOffset, self.titleLabel.frame.size.width, 15.0f)];
+	yOffset = (showArtist) ? self.artistLabel.frame.origin.y + self.artistLabel.frame.size.height : yOffset;
+	[self.categoryLabel setFrame:CGRectMake(self.artistLabel.frame.origin.x, yOffset, self.titleLabel.frame.size.width, 15.0f)];
+	[self.summaryLabel setFrame:CGRectMake(self.categoryLabel.frame.origin.x, 63.0f, self.titleLabel.frame.size.width, 50.0f)];
+	
 }
 
 #pragma mark - enable / disable parent and siblings
